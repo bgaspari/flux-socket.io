f9887aac0328834a3c571828e690f88d099790a4
bgaspari <bgaspari@gmail.com>
2016-03-24 15:35:53 +0100

    First commit
    Create repository
    first module version

---
+++ index.js
@@ -0,0 +1,4 @@
+export dispatcherWS from './lib/dispatcherWS'; 
+export storeWSClient from './lib/storeWSClient';
+export storeWSServer from './lib/storeWSServer';
+export ReactConnectWS from './lib/reactConnectWS';
