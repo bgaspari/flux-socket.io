3d61ec29a8a21d7fc3dc80d4e944aec1661b7e17
bgaspari <bgaspari@gmail.com>
2016-04-01 16:24:23 +0200

    ADD ERROR CONSTANT

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 export dispatcherWS from './lib/dispatcherWS'; 
 export storeWSClient from './lib/storeWSClient';
 export storeWSServer from './lib/storeWSServer';
+export { SERVER_ERROR } from './lib/storeWSServer';
 export ConnectWSStores from './lib/connectWSStores';
